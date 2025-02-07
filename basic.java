24973be635d315cdefae0293c641679d0ebc9c36
Rahad Islam <rahatislam2100@gmail.com>
2025-02-07 19:24:51 +0600

    Update basic.java

--- basic.java
+++ basic.java
@@ -5,3 +5,14 @@ class Test
         System.out.println("My First Java Program.");
     }
 }
+package com.ok.java.datatypes;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+        byte b=1;
+        short c=1;
+        long d=1;
+        System.out.println(c+d);
+    }
+}
