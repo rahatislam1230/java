a61b1676bb9d38158eb0748dce5bde88490b8faa
Rahad Islam <rahatislam2100@gmail.com>
2025-02-08 22:20:04 +0600

    Update basic.java

--- basic.java
+++ basic.java
@@ -182,3 +182,23 @@ class Test {
 
 }
 }
+import java.util.Scanner;
+
+class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] n = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < n.length; ++i) {
+            n[i] = sc.nextInt();  
+        }
+
+        System.out.println("You entered:");
+        for (int i = 0; i < n.length; ++i) {
+            System.out.println(n[i]);
+        }
+
+        sc.close();
+    }
+}
