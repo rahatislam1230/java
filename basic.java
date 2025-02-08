0653d6ee0cacec832fb9bd0584ae5d78b9eb960b
Rahad Islam <rahatislam2100@gmail.com>
2025-02-08 21:46:31 +0600

    Update basic.java

--- basic.java
+++ basic.java
@@ -120,3 +120,18 @@ public class test {
         }
     }
 }
+import java.util.Scanner;
+
+class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println(n+"eve");
+        } else {
+            System.out.println(n+"odd");
+        }
+        sc.close();
+    }
+}
