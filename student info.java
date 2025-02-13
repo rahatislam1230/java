d7b0f107e3298dce3c00d9c8fae6d215f2605bd0
Rahad Islam <rahatislam2100@gmail.com>
2025-02-14 00:03:34 +0600

    Update student info.java

--- student info.java
+++ student info.java
@@ -27,3 +27,33 @@ class Student {
         s.display();
     }
 }
+import java.util.Scanner;
+
+public class teacher {
+    String name;
+    float balance;
+    String rank;
+    int dep;
+    private void input(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name");
+        name= input.nextLine();
+        System.out.println("Enter the balace");
+        balance=input.nextFloat();
+        System.out.println("Enter the rank ");
+        rank=input.nextLine();
+        System.out.println("Enter dep");
+        dep=input.nextInt();
+    }
+    private void detaisl(){
+        System.out.println("name"+name);
+        System.out.println("balance"+balance);
+        System.out.println("rank"+rank);
+        System.out.println("dep"+dep);
+    }
+    public static void main(String[] args){
+        teacher t = new teacher();
+        t.input();
+        t.detaisl();
+    }
+}
