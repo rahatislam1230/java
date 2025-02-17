061e007ab5279df39fefa613599273ad2afdff04
Rahad Islam <rahatislam2100@gmail.com>
2025-02-17 18:51:59 +0600

    Update normal.java

--- normal.java
+++ normal.java
@@ -29,3 +29,17 @@ public class funtion {
         printMyName(name);
     }    
 }
+import java.util.Scanner;
+
+    public  static  int calculateSum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int sum = calculateSum(a,b);
+        System.out.println(sum);
+
+    }    
+
