563ae98d3807d43a5824016388a526e37c65c9e5
Rahad Islam <rahatislam2100@gmail.com>
2025-02-17 19:11:30 +0600

    Update normal.java

--- normal.java
+++ normal.java
@@ -92,4 +92,21 @@ public class Array {
     }
 
 }
+import java.util.Scanner;
+public class Array {
 
+    public static void main(String[] args) {
+        int[] marks= new int [3];
+        Scanner sc= new Scanner(System.in);
+        System.out.println("marks");
+
+
+        for(int i=0;i<3;i++){
+            marks[i]= sc.nextInt();
+        }
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+
+}
