1e0778d38b7538978891758fc479d852548b935e
Rahad Islam <rahatislam2100@gmail.com>
2025-02-17 18:54:24 +0600

    Update normal.java

--- normal.java
+++ normal.java
@@ -42,4 +42,20 @@ import java.util.Scanner;
         System.out.println(sum);
 
     }    
+import java.util.Scanner;
+
+   // public  static  int calculateSum(int a,int b){
+       // return a+b;
+   // }
+public static  int calculateProduct(int a,int b){
+    return a*b;
+}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(calculateProduct(a,b));
+
+    }    
+
 
