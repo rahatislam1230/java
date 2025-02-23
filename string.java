d794427a067312a54a729c4b78657febb6b70970
Rahad Islam <rahatislam2100@gmail.com>
2025-02-23 21:50:29 +0600

    Update string.java

--- string.java
+++ string.java
@@ -34,3 +34,16 @@ public class Strings{
     }
 
 }
+import java.util.Scanner;
+public class Strings{
+
+
+    public static void main(String[] args) {
+       // concatenation
+        String firstName="tony";
+        String lastName ="stark";
+        String fullName= firstName+ " "+lastName;
+        System.out.println(fullName);
+    }
+
+}
