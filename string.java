218d883fe8e8e9f241d2ba69fdcbebe494439c6d
Rahad Islam <rahatislam2100@gmail.com>
2025-02-23 21:45:40 +0600

    Create string.java

---
+++ string.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Strings{
+
+
+    public static void main(String[] args) {
+        String name ="tony";
+        String fullname="tony stark";
+        String sectence= "my name tony stark";
+        System.out.println(name);
+    }
+
+}
