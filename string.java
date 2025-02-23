aed8557f1c868a6d3c89259211363e5d25a61817
Rahad Islam <rahatislam2100@gmail.com>
2025-02-23 21:47:21 +0600

    Update string.java

--- string.java
+++ string.java
@@ -10,3 +10,14 @@ public class Strings{
     }
 
 }
+import java.util.Scanner;
+public class Strings{
+
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String name = sc.next();
+        System.out.println("your name " +  name);
+    }
+
+}
