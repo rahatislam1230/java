a63eb3bd6064cc28ad36ce3f1ebdaa2a8c35edf2
Rahad Islam <rahatislam2100@gmail.com>
2025-02-23 22:00:10 +0600

    Update string.java

--- string.java
+++ string.java
@@ -98,3 +98,24 @@ public class Strings{
     }
 
 }
+import java.util.Scanner;
+public class Strings{
+
+
+    public static void main(String[] args) {
+       // compare
+        String name1="tony";
+        String name2 ="tony";
+        // 1 s1>s2 : +ve value
+        // 2 s1==s2: 0
+        // 3 s1<s2: -ve value
+        if(name1.compareTo(name2)==0){
+            System.out.println("strings are equal");
+
+        }else{
+            System.out.println("Strings are not are equal");
+        }
+
+    }
+
+}
