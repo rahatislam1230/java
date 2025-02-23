63cc48cc735f00d2dc74f9ef16e169972cd3734d
Rahad Islam <rahatislam2100@gmail.com>
2025-02-23 22:15:45 +0600

    Update string.java

--- string.java
+++ string.java
@@ -225,3 +225,23 @@ public class Strings{
         System.out.println(sb);
     }
 }
+import java.util.Scanner;
+public class Strings{
+
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+        //char at index 0
+        System.out.println(sb.charAt(0));
+        // set char at index 0
+        sb.setCharAt(0,'P');
+        System.out.println(sb);
+        sb.insert(0,'s');
+        System.out.println(sb);
+        // detete the extra char
+        sb.delete(2,3);
+        System.out.println(sb);
+    }
+}
