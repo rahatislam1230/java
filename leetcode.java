1b21c05ad38a000105ea0aa8339631ef7a04edc2
Rahad Islam <rahatislam2100@gmail.com>
2025-02-28 16:05:19 +0600

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -124,3 +124,20 @@ class Solution {
         return -1;
     }
 }
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+         int low=0;
+         int high=arr.length-1;
+         while(low<high){
+            int mid =low + (high-low)/2;
+            if(arr[mid]< arr[mid + 1]){
+                low= mid+1;
+            }else{
+                high =mid;
+            }
+         }
+         return low;
+   }
+
+
+}
