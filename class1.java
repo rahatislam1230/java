24cd14a8988cd4dd99f9847ff1f0ffd2775cc1be
Rahad Islam <rahatislam2100@gmail.com>
2025-03-07 16:07:25 +0600

    Update class1.java

--- class1.java
+++ class1.java
@@ -23,3 +23,27 @@ class Funtion{
 
 	}
 }
+import java.util.*;
+import java.lang.*;
+class Funtion{
+	 public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 int a = sc.nextInt();
+	 int b=sc.nextInt();
+	 int c= sc.nextInt();
+	 if(a>b){
+	 	if(a>c){
+	 		System.out.println("a is the biggest");
+	 	}else{
+	 		System.out.println("c is the biggest");
+	 	}
+	 }else{
+	 	if(b>c){
+	 		System.out.println("b is the biggest");
+	 	}else{
+	 		System.out.println("c is the biggest");
+	 	}
+	 }
+
+	}
+}
