999ffbb0a71ecb6ff0e4dc61a93814e4107102ad
Rahad Islam <rahatislam2100@gmail.com>
2025-03-25 19:42:56 +0600

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -141,3 +141,17 @@ class Solution {
 
 
 }
+344
+ class Solution {
+    public void reverseString(char[] s) {
+        int start=0;
+        int end = s.length-1;
+        while(start<=end){
+            char temp=s[start];
+            s[start]=s[end];
+            s[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
