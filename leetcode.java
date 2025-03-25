063f33fb2104bb84c24b6c775df8fe178ab00d2d
Rahad Islam <rahatislam2100@gmail.com>
2025-03-25 19:50:01 +0600

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -155,3 +155,15 @@ class Solution {
         }
     }
 }
+171
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        char c []= columnTitle.toCharArray();
+        int result =0;
+        for(int i=0;i<c.length;i++){
+            int num = c[i]-65+1;
+            result = result *26+num;
+        }
+        return result;
+    }
+}
