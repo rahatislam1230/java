9b0b62507faf271dacaab240e4a97f1d162fff9b
Rahad Islam <rahatislam2100@gmail.com>
2025-04-08 21:53:00 +0600

    Update basic.java

--- basic.java
+++ basic.java
@@ -766,3 +766,28 @@ class Start{
 		 }
 	 }
 }
+class Student {
+    public Student(){
+        System.out.println("welcome");
+    }
+}
+class School{
+    public static void main(String[]args){
+        Student s = new Student();
+    }
+}
+class Counter{
+          static int count =0;
+          public Counter(){
+            count++;
+            System.out.println(count);
+          }
+}
+public class Main{
+public static void main(String[]args){
+    Counter c1=new Counter();
+    Counter c2=new Counter();
+    Counter c3=new Counter();
+    System.out.println("Total objects created: " + Counter.count);
+
+}}
